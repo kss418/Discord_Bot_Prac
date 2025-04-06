6a4665fa40fa4c0e2031d73b50e852c37bfed5eb
kss418 <jis1595@naver.com>
2025-04-06 16:50:32 +0900

    디스코드 봇 커맨드 벡터로 변경

--- src/Discord_BOT.cpp
+++ src/Discord_BOT.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <vector>
 
-const dpp::snowflake Guild_ID = 1240679613072539709;
+const dpp::snowflake Guild_ID = 408601520951656448;
 
 Discord_BOT::Discord_BOT(const std::string& API_Key, const Maple_API& M_API) : BOT(API_Key), M_API(M_API){
     Create_Command();
@@ -30,10 +30,12 @@ void Discord_BOT::Create_Command(){
     BOT.on_ready([this](const dpp::ready_t& Event){
         std::cout << "봇이 성공적으로 켜졌습니다" << std::endl;
         //if(dpp::run_once<struct register_bot_commands>()){
-            auto CMD_Get_Info = dpp::slashcommand("정보", "사진 출력", BOT.me.id)
-                .add_option(dpp::command_option(dpp::co_string, "character_name", "닉네임", true));
+            std::vector<dpp::slashcommand> Commands = {
+                dpp::slashcommand("정보", "사진 출력", BOT.me.id)
+                    .add_option(dpp::command_option(dpp::co_string, "character_name", "닉네임", true))
+            };
 
-            Add_Command_Guild(CMD_Get_Info);
+            for(const auto& CMD : Commands) Add_Command_Guild(CMD);
         //}
     });
 }
