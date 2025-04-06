df58d640b314efc7192adee660c9ce1280552f63
kss418 <jis1595@naver.com>
2025-04-06 20:58:59 +0900

    메이플 API 파서 분할

---
+++ src/Maple_API_Parser.cpp
@@ -0,0 +1,91 @@
+#include "Maple_API.h"
+#include "Custom_Class.h"
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <cpr/cpr.h>
+
+std::string Get_Str(const nlohmann::json& Data, const std::string& Key) {
+    if (!Data.contains(Key) || Data[Key].is_null()) return "";
+    return Data[Key].get<std::string>();
+}
+
+int32_t Get_Int32(const nlohmann::json& Data, const std::string& Key) {
+    if (!Data.contains(Key) || Data[Key].is_null()) return 0;
+    return Data[Key].get<int32_t>();
+}
+
+int64_t Get_Int64(const nlohmann::json& Data, const std::string& Key) {
+    if (!Data.contains(Key) || Data[Key].is_null()) return 0;
+    return Data[Key].get<int64_t>();
+}
+
+void Maple_API::Parse_Potential_Option_Info(Potential_Option& Info, const nlohmann::json& Data) const {
+    Info.Grade = Get_Str(Data, "potential_option_grade");
+    Info.Option[0] = Get_Str(Data, "potential_option_1");
+    Info.Option[1] = Get_Str(Data, "potential_option_2");
+    Info.Option[2] = Get_Str(Data, "potential_option_3");
+}
+
+void Maple_API::Parse_Additional_Potential_Option_Info(Potential_Option& Info, const nlohmann::json& Data) const {
+    Info.Additional_Grade = Get_Str(Data, "additional_potential_option_grade");
+    Info.Additional_Option[0] = Get_Str(Data, "additional_potential_option_1");
+    Info.Additional_Option[1] = Get_Str(Data, "additional_potential_option_2");
+    Info.Additional_Option[2] = Get_Str(Data, "additional_potential_option_3");
+}
+
+void Maple_API::Parse_Option_Info(Option& Info, const nlohmann::json& Data) const {
+    Info.Str = Get_Str(Data, "str");
+    Info.Dex = Get_Str(Data, "dex");
+    Info.Int = Get_Str(Data, "int");
+    Info.Luk = Get_Str(Data, "luk");
+    Info.Max_HP = Get_Str(Data, "max_hp");
+    Info.Max_MP = Get_Str(Data, "max_mp");
+    Info.Attack_Power = Get_Str(Data, "attack_power");
+    Info.Magic_Power = Get_Str(Data, "magic_power");
+    Info.Boss_Damage = Get_Str(Data, "boss_damage");
+    Info.All_Stat = Get_Str(Data, "all_stat");
+    Info.Damage = Get_Str(Data, "damage");
+    Info.Max_HP_Rate = Get_Str(Data, "max_hp_rate");
+    Info.Max_MP_Rate = Get_Str(Data, "max_mp_rate");
+}
+
+void Maple_API::Parse_Equipment_Info(std::vector<Equipment_Info>& Info, const nlohmann::json& Data) const {
+    for (const auto& Current_Equipment : Data) {
+        Equipment_Info Now;
+        Now.Part_Name = Get_Str(Current_Equipment, "item_equipment_part");
+        Now.Slot_Name = Get_Str(Current_Equipment, "item_equipment_slot");
+        Now.Item_Name = Get_Str(Current_Equipment, "item_name");
+        Now.Item_Icon = Get_Str(Current_Equipment, "item_icon");
+        Now.Uprage_Count = Get_Str(Current_Equipment, "scroll_upgrade");
+        Now.Golden_Hammer_Flag = Get_Str(Current_Equipment, "golden_hammer_flag");
+
+        if (Now.Slot_Name == "무기") {
+            Now.Soul_Name = Get_Str(Current_Equipment, "soul_name");
+            Now.Soul_Option = Get_Str(Current_Equipment, "soul_option");
+        }
+
+        Now.Starforce = Get_Str(Current_Equipment, "starforce");
+
+        if (Now.Part_Name == "반지")
+            Now.Special_Ring_Level = Get_Int32(Current_Equipment, "special_ring_level");
+
+        if (Current_Equipment.contains("item_total_option"))
+            Parse_Option_Info(Now.Total_Option, Current_Equipment["item_total_option"]);
+        if (Current_Equipment.contains("item_base_option"))
+            Parse_Option_Info(Now.Base_Option, Current_Equipment["item_base_option"]);
+        if (Current_Equipment.contains("item_additional_option"))
+            Parse_Option_Info(Now.Additional_Option, Current_Equipment["item_additional_option"]);
+        if (Current_Equipment.contains("item_etc_option"))
+            Parse_Option_Info(Now.Exceptional_Option, Current_Equipment["item_etc_option"]);
+
+        Now.Flag = Get_Str(Current_Equipment, "potential_option_flag");
+        if (Now.Flag == "false")
+            Parse_Potential_Option_Info(Now.Basic_Potential_Option, Current_Equipment);
+        
+        Now.Additional_Flag = Get_Str(Current_Equipment, "additional_potential_option_flag");
+        if (Now.Additional_Flag == "false")
+            Parse_Additional_Potential_Option_Info(Now.Additional_Potential_Option, Current_Equipment);
+
+        Info.push_back(Now);
+    }
+}
